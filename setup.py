6e36d0c82b4c21103db81eeeca553069179d9026
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-09-30 16:53:41 +0200

    new release on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="drawarrow",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     description="Drawing arrows for matplotlib made easy.",
     long_description=open("README.md").read(),
