de577690b752607fd6f520148784ddb47c3a54e8
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-03-18 17:37:14 +0100

    wip arrow spines

--- docs/reference/arrow_spines.md
+++ docs/reference/arrow_spines.md
@@ -18,6 +18,24 @@ ax.scatter([1, 2, 3, 8, 6, 10], [2, 5, 3, 9, 2, 10])
 arrow_spines(ax=ax, color="red")
 ```
 
+```python
+# mkdocs: render
+import matplotlib.pyplot as plt
+from drawarrow import arrow_spines
+
+fig, ax = plt.subplots()
+
+ax.scatter([1, 2, 3, 8, 6, 10], [2, 5, 3, 9, 2, 10])
+
+arrow_spines(
+   right="totop",
+   bottom="toleft",
+   left=None, # remove left spine
+   ax=ax,
+   color="red",
+)
+```
+
 <br>
 
 [Going further](https://python-graph-gallery.com/drawarrow/)
--- drawarrow/arrow_spines.py
+++ drawarrow/arrow_spines.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from typing import Union
 from drawarrow import ax_arrow
 
 
-def arrow_spines(ax=None, **arrow_style):
+def arrow_spines(
+    bottom="toright",
+    left="totop",
+    right=None,
+    top=None,
+    ax: Union[Axes, None] = None,
+    **arrow_style,
+):
     """
     Remove spines in a matplotlib Axes and add arrows (left to right and bottom to top) instead.
 
@@ -26,15 +35,57 @@ def arrow_spines(ax=None, **arrow_style):
     )
     default_arrow_style.update(arrow_style)
 
-    # left to right arrow
-    ax_arrow([0 - 0.005, 0], [1.02, 0], **default_arrow_style)
+    if bottom is not None:
+        if bottom == "toright":
+            # left to right arrow
+            ax_arrow([0 - 0.005, 0], [1.02, 0], **default_arrow_style)
+        elif bottom == "toleft":
+            # right to left arrow
+            ax_arrow([1.02, 0], [0 - 0.005, 0], **default_arrow_style)
+        else:
+            raise ValueError(
+                f"bottom must be one of: 'toright', 'toleft', None, not {bottom}"
+            )
 
-    # bottom to top arrow
-    ax_arrow([0, 0 - 0.005], [0, 1.02], **default_arrow_style)
+    if left is not None:
+        if left == "totop":
+            # bottom to top arrow
+            ax_arrow([0, 0 - 0.005], [0, 1.02], **default_arrow_style)
+        elif left == "tobottom":
+            # top to bottom arrow
+            ax_arrow([0, 1.02], [0, 0 - 0.005], **default_arrow_style)
+        else:
+            raise ValueError(
+                f"left must be one of: 'totop', 'tobottom', None, not {left}"
+            )
+
+    if top is not None:
+        if top == "toleft":
+            # right to left arrow
+            ax_arrow([1, 1], [0 - 0.005, 1], **default_arrow_style)
+        elif top == "toright":
+            # left to right arrow
+            ax_arrow([0 - 0.005, 1], [1, 1], **default_arrow_style)
+        else:
+            raise ValueError(
+                f"top must be one of: 'toright', 'toleft', None, not {top}"
+            )
+
+    if right is not None:
+        if right == "tobottom":
+            # top to bottom arrow
+            ax_arrow([1, 1.02], [1, 0 - 0.005], **default_arrow_style)
+        elif right == "totop":
+            # bottom to top arrow
+            ax_arrow([1, 0 - 0.005], [1, 1.02], **default_arrow_style)
+        else:
+            raise ValueError(
+                f"right must be one of: 'totop', 'tobottom', None, not {right}"
+            )
 
 
 if __name__ == "__main__":
     fig, ax = plt.subplots()
     ax.scatter([1, 2, 3, 8, 6, 10], [2, 5, 3, 9, 2, 10])
-    arrow_spines(ax, color="red")
+    arrow_spines(ax=ax, color="red")
     fig.savefig("cache.png", dpi=300)
