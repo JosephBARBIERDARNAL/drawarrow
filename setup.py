4601a3e1435b0335a94ec0a55211151d0a691795
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-03-18 21:36:49 +0100

    fix link

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="drawarrow",
     version="0.1.0",
-    packages=find_packages(),
+    packages=["drawarrow"],
     description="Drawing arrows for matplotlib made easy.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
