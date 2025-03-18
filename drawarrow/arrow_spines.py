dd1aee9dca98abea35ef17a6e75710867d4b4d3a
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-03-18 20:42:58 +0100

    fix arrow_spines

--- drawarrow/arrow_spines.py
+++ drawarrow/arrow_spines.py
@@ -41,10 +41,10 @@ def arrow_spines(
             ax_arrow([0 - 0.005, 0], [1.02, 0], **default_arrow_style)
         elif bottom == "toleft":
             # right to left arrow
-            ax_arrow([1.02, 0], [0 - 0.005, 0], **default_arrow_style)
+            ax_arrow([1 + 0.005, 0], [0 - 0.005, 0], **default_arrow_style)
         else:
             raise ValueError(
-                f"bottom must be one of: 'toright', 'toleft', None, not {bottom}"
+                f"bottom must be one of: 'toright', 'toleft', None, not '{bottom}'"
             )
 
     if left is not None:
@@ -53,39 +53,46 @@ def arrow_spines(
             ax_arrow([0, 0 - 0.005], [0, 1.02], **default_arrow_style)
         elif left == "tobottom":
             # top to bottom arrow
-            ax_arrow([0, 1.02], [0, 0 - 0.005], **default_arrow_style)
+            ax_arrow([0, 1], [0, 0 - 0.005], **default_arrow_style)
         else:
             raise ValueError(
-                f"left must be one of: 'totop', 'tobottom', None, not {left}"
+                f"left must be one of: 'totop', 'tobottom', None, not '{left}'"
             )
 
     if top is not None:
         if top == "toleft":
             # right to left arrow
-            ax_arrow([1, 1], [0 - 0.005, 1], **default_arrow_style)
+            ax_arrow([1 + 0.005, 1], [0 - 0.005, 1], **default_arrow_style)
         elif top == "toright":
             # left to right arrow
-            ax_arrow([0 - 0.005, 1], [1, 1], **default_arrow_style)
+            ax_arrow([0 - 0.005, 1], [1 + 0.005, 1], **default_arrow_style)
         else:
             raise ValueError(
-                f"top must be one of: 'toright', 'toleft', None, not {top}"
+                f"top must be one of: 'toright', 'toleft', None, not '{top}'"
             )
 
     if right is not None:
         if right == "tobottom":
             # top to bottom arrow
-            ax_arrow([1, 1.02], [1, 0 - 0.005], **default_arrow_style)
+            ax_arrow([1, 1], [1, 0 - 0.005], **default_arrow_style)
         elif right == "totop":
             # bottom to top arrow
             ax_arrow([1, 0 - 0.005], [1, 1.02], **default_arrow_style)
         else:
             raise ValueError(
-                f"right must be one of: 'totop', 'tobottom', None, not {right}"
+                f"right must be one of: 'totop', 'tobottom', None, not '{right}'"
             )
 
 
 if __name__ == "__main__":
     fig, ax = plt.subplots()
     ax.scatter([1, 2, 3, 8, 6, 10], [2, 5, 3, 9, 2, 10])
-    arrow_spines(ax=ax, color="red")
+    arrow_spines(
+        right=None,
+        bottom=None,
+        left="totop",
+        top=None,
+        ax=ax,
+        color="red",
+    )
     fig.savefig("cache.png", dpi=300)
